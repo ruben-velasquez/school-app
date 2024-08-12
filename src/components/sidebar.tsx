c7d63b65e8153c67bfd6c656bd3288fe3ba67bf4
ruben-velasquez <rualvemo@hotmail.com>
2024-08-11 20:56:50 -0400

    Add icon to the title

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,14 @@
-import { BiBook, BiHome, BiNote } from "react-icons/bi";
+import { BiBarChartAlt2, BiBook, BiHome, BiNote } from "react-icons/bi";
 import SidebarButton from "./sidebar-button";
 
 export default function Sidebar() {
     return (
         <aside className="bg-sidebar w-1/5 flex flex-col gap-5 py-5 font-main">
             <header className="px-5">
-                <h1 className="font-bold text-xl">School Management</h1>
+                <h1 className="font-bold text-xl flex gap-2">
+                    <BiBarChartAlt2 />
+                    School Management
+                </h1>
             </header>
             <ul className="display flex flex-col gap-2 px-2 justify-center text-gray-400">
                 <SidebarButton href="#"><BiHome size={20} /> Home</SidebarButton>
