cfb33ff1f730fcc47bcbe342ac4b1302e8aaf896
ruben-velasquez <rualvemo@hotmail.com>
2024-08-24 19:03:18 -0400

    Make tasks number work in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,9 @@ import BoxSection from "@/components/box-section";
 import Button from "@/components/button";
 import CourseSchedule from "@/components/course-schedule";
 import Sidebar from "@/components/sidebar";
+import TasksNumber from "@/components/tasks-number";
 import {
-  BiLeaf,
-  BiMath,
   BiMedal,
-  BiSolidCalendarExclamation,
-  BiSolidMegaphone,
-  BiSolidPizza,
   BiSolidRightArrowAlt,
   BiTask,
 } from "react-icons/bi";
@@ -37,7 +33,7 @@ export default function Home() {
           <BoxSection>
             <span className="text-lg text-gray-300">Pending tasks:</span>
             <h3 className="text-4xl font-bold flex gap-2 mb-2">
-              <BiTask /> 3
+              <BiTask /> <TasksNumber />
             </h3>
             <Button href="#">
               Go to tasks <BiSolidRightArrowAlt size={24} />
---
+++ src/components/tasks-number.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useAppSelector } from "@/redux/hooks"
+
+export default function TasksNumber() {
+  const tasks = useAppSelector(state => state.tasks)
+
+  return (
+    <span>
+      {tasks.value.length}
+    </span>
+  )
+}
